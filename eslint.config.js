3e407fd7128d0ec8bac383eb7e87374a05a34ef1
techxplorer <corey@techxplorer.com>
2024-07-30 17:51:36 +0930

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,53 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
+import { default as jquery } from "eslint-config-jquery";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: [
+      "bin/*.js"
+    ]
+  },
   pluginJs.configs.recommended,
+  jsdoc.configs[ "flat/recommended" ],
+  jquery,
+  {
+    languageOptions: {
+      globals: globals.nodeBuiltin
+    },
+    files: [
+      "src/**/*.js"
+    ],
+    rules:
+    {
+      indent: [
+        "error",
+        2
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "jsdoc/check-indentation": [
+        "warn"
+      ],
+      "jsdoc/check-line-alignment": [
+        "warn"
+      ],
+      "jsdoc/require-description": [
+        "warn"
+      ],
+      "jsdoc/require-description-complete-sentence": [
+        "warn"
+      ],
+      "jsdoc/require-file-overview": [
+        "warn"
+      ],
+      "jsdoc/require-throws": [
+        "warn"
+      ]
+    }
+  }
 ];
