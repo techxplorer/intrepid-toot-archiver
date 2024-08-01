f04438968a41b54cefd635b551ddb577bf7eb4e8
techxplorer <corey@techxplorer.com>
2024-08-01 11:11:55 +0930

    Complete first version of PackageUtils class

--- src/utils/package-utils.js
+++ src/utils/package-utils.js
@@ -31,9 +31,59 @@ export default class PackageUtils {
   /**
    * Get the version property from the package.json file.
    * @returns {string} The version of the app.
+   * @throws {Error} When the property is not defined.
    */
   getVersion() {
+
+    if ( typeof this.appPackage.version === "undefined" ) {
+      this.throwError( "version number" );
+    }
+
     return this.appPackage.version;
   }
 
+  /**
+   * Get the description property from the package.json file.
+   * @returns {string} The description of the app.
+   * @throws {Error} When the property is not defined.
+   */
+  getDescription() {
+
+    if ( typeof this.appPackage.description === "undefined" ) {
+      this.throwError( "description" );
+    }
+
+    return this.appPackage.description;
+  }
+
+  /**
+   * Get the homepage property from the package.json file.
+   * @returns {string} The homepage of the app.
+   * @throws {Error} When the property is not defined.
+   */
+  getHomepage() {
+
+    if ( typeof this.appPackage.homepage === "undefined" ) {
+      this.throwError( "homepage" );
+    }
+
+    try {
+      new URL( this.appPackage.homepage );
+    // eslint-disable-next-line no-unused-vars
+    } catch ( err ) {
+      this.throwError( "homepage" );
+    }
+
+    return this.appPackage.homepage;
+  }
+
+  /**
+   * Make it easy to throw a consistent error message.
+   * @param {string} propertyName The name of the property that wasn't found.
+   */
+  throwError( propertyName ) {
+    throw new Error(
+      `Unable to get ${ propertyName } from package.json`
+    );
+  }
 }
--- tests/utils/package-utils.test.js
+++ tests/utils/package-utils.test.js
@@ -3,10 +3,12 @@ import { describe, it } from "node:test";
 
 import { default as semverValid } from "semver/functions/valid.js";
 
-import PackageUtils from "../..//src/utils/package-utils.js";
+import PackageUtils from "../../src/utils/package-utils.js";
 
 describe( "PackageUtils", () => {
+
   describe( "getVersion", () => {
+
     it( "should report a valid version number", () => {
       const appPackage = new PackageUtils();
       assert.notEqual(
@@ -14,5 +16,96 @@ describe( "PackageUtils", () => {
         null
       );
     } );
+
+    it( "shoould not throw an error", () => {
+      const appPackage = new PackageUtils();
+      assert.doesNotThrow(
+        () => {
+          appPackage.getVersion();
+        }
+      );
+    } );
+
+    it( "shoould throw an error when the requested field is not found", () => {
+      const appPackage = new PackageUtils();
+      appPackage.appPackage.version = undefined;
+      assert.throws(
+        () => {
+          appPackage.getVersion();
+        }
+      );
+    } );
+
+  } );
+
+  describe( "getDescription", () => {
+
+    it( "should return a valid description", () => {
+      const appPackage = new PackageUtils();
+      assert.equal(
+        appPackage.getDescription(),
+        "My MicroFiction Archiver"
+      );
+    } );
+
+    it( "shoould not throw an error", () => {
+      const appPackage = new PackageUtils();
+      assert.doesNotThrow(
+        () => {
+          appPackage.getDescription();
+        }
+      );
+    } );
+
+    it( "should throw an error when the requested field is not found", () => {
+      const appPackage = new PackageUtils();
+      appPackage.appPackage.description = undefined;
+      assert.throws(
+        () => {
+          appPackage.getDescription();
+        }
+      );
+    } );
+  } );
+
+  describe( "getHomepage", () => {
+
+    it( "should return a valid homepage url", () => {
+      const appPackage = new PackageUtils();
+      assert.equal(
+        appPackage.getHomepage(),
+        "https://github.com/techxplorer/"
+      );
+    } );
+
+    it( "shoould not throw an error", () => {
+      const appPackage = new PackageUtils();
+      assert.doesNotThrow(
+        () => {
+          appPackage.getHomepage();
+        }
+      );
+    } );
+
+    it( "should throw an error when the requested field is not found", () => {
+      const appPackage = new PackageUtils();
+      appPackage.appPackage.homepage = undefined;
+      assert.throws(
+        () => {
+          appPackage.getHomepage();
+        }
+      );
+    } );
+
+    it( "should throw an error when the requested field is not valid", () => {
+      const appPackage = new PackageUtils();
+      appPackage.appPackage.homepage = "not a valid url";
+      assert.throws(
+        () => {
+          appPackage.getHomepage();
+        }
+      );
+    } );
+
   } );
 } );
