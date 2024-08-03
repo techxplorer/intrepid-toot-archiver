423d0f2cee1b394953cfb6527c481e9a108c88cf
techxplorer <corey@techxplorer.com>
2024-08-03 13:00:07 +0930

    Start adding lookup user command

--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,14 @@
 import { Command } from 'commander';
 import prettyMS from "pretty-ms";
 
+import LookupUser from "../src/commands/lookup-user.js";
 import PackageUtils from "../src/utils/package-utils.js";
 
 const startTime = process.hrtime.bigint();
 
 async function run() {
 
-  // Setup the program
+  // Setup the program.
   const program = new Command();
   const appPackage = new PackageUtils();
   program
@@ -19,7 +20,15 @@ async function run() {
     .addHelpText(
       'after',
       `\nMore info: ${ appPackage.getHomepage() }\nVersion: ${ appPackage.getVersion() }`
-    )
+    );
+
+  // Add the lookup user command.
+  program.command( "lookup-user" )
+    .description( "Lookup user details on a Mastodon host" )
+    .action( async () => {
+      const lookupUser = new LookupUser();
+      await lookupUser.run();
+    } );
 
   // Parse the command line parameters.
   await program.parseAsync( process.argv );
@@ -27,7 +36,7 @@ async function run() {
   const endTime = process.hrtime.bigint();
   const totalTime = Number( endTime - startTime ) * 1e-6;
 
-  console.log( "Elapsed time:", prettyMS( totalTime ) );
+  console.log( "\nElapsed time:", prettyMS( totalTime ) );
 }
 
 run();
---
+++ src/commands/lookup-user.js
@@ -0,0 +1,63 @@
+/**
+ * @file The defintition of the lookup-user command class.
+ */
+
+import chalk from "chalk";
+import { input, confirm } from "@inquirer/prompts";
+import isFQDN from "validator/lib/isFQDN.js";
+import matches from "validator/lib/matches.js";
+
+/**
+ * Make available various properties from the package.json file, and
+ * provide simple methods to explore unit testing.
+ */
+export default class LookupUser {
+
+  /**
+   * Run the command to lookup user details.
+   */
+  async run() {
+
+    console.log( chalk.bold( "Collecting required info..." ) );
+
+    const host = await input( {
+      message: "Enter the domain name of the host",
+      required: true,
+      validate: ( value = "" ) => isFQDN( value ) || "Enter valid domain name"
+
+    } );
+
+    const userName = await input( {
+      message: "Enter the user name",
+      required: true,
+      validate: ( value = "" ) => matches( value, /^@?([A-Z0-9._%+-]+)/ ) || "Enter valid user name"
+    } );
+
+    console.log( chalk.green( "Collected information" ) );
+    console.table( [
+      {
+        name: "Server host",
+        value: host
+      },
+      {
+        name: "User name",
+        value: userName
+      }
+    ] );
+
+    const useConfig = await confirm( {
+      message: "Lookup user using collected information",
+      default: true
+    } );
+
+    if ( useConfig !== true ) {
+      return;
+    }
+
+    console.log( chalk.bold( "Looking up user..." ) );
+
+    //console.log( `Server host: ${ host }\nUser name:   ${ userName }` );
+
+  }
+
+}
